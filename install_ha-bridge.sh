c653084ed7dba915a3587fc9db23884c7254e810
a-p-robinson <a.p.robinson@gmail.com>
2020-12-06 21:45:10 +0000

    virtualenv setup

--- install_ha-bridge.sh
+++ install_ha-bridge.sh
@@ -16,13 +16,13 @@ vers="5.3.0"
 
 cd /opt/ha-bridge
 sudo wget https://github.com/bwssytems/ha-bridge/releases/download/v$vers/ha-bridge-$vers.jar
-
 sudo mv ha-bridge-$vers.jar ha-bridge.jar
 
 # Install the systemd service
 sudo cp $DIR/config/ha-bridge.service /etc/systemd/system/
-
 sudo systemctl daemon-reload
 sudo systemctl start ha-bridge.service
 sudo systemctl enable ha-bridge.service
+
+
 
