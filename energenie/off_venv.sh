f2ce26c659d4324b58fadcbf7584e5d707451522
a-p-robinson <a.p.robinson@gmail.com>
2020-12-07 16:59:21 +0000

    Pass socket number as a parameter to veneve script

--- energenie/off_venv.sh
+++ energenie/off_venv.sh
@@ -4,7 +4,7 @@
 source /home/apr/rpi-homeautomation/energenie/env/bin/activate
 
 ## DO SOME STUFF -> USE FULL PATH HERE TOO #
-python /home/apr/rpi-homeautomation/energenie/switch_off.py 4 > /home/apr/rpi-homeautomation/energenie/log.log
+python /home/apr/rpi-homeautomation/energenie/switch_off.py $1 > /home/apr/rpi-homeautomation/energenie/log.log
 
 deactivate
 
